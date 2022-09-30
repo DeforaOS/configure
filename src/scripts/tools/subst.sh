85ca35f9f4024e27098eb2c9eddf20c99382c125
Pierre Pronchery <khorben@defora.org>
2022-09-30 11:58:34 +0200

    scripts/tools/subst.sh: also substitute VENDOR

--- src/scripts/tools/subst.sh
+++ src/scripts/tools/subst.sh
@@ -117,7 +117,8 @@ _subst()
 		source="${source}.in"
 		([ -z "$OBJDIR" ] || $DEBUG $MKDIR -- "${target%/*}") \
 								|| return 2
-		$DEBUG $SED -e "s;@PACKAGE@;$PACKAGE;g" \
+		$DEBUG $SED -e "s;@VENDOR@;$VENDOR;g" \
+			-e "s;@PACKAGE@;$PACKAGE;g" \
 			-e "s;@VERSION@;$VERSION;g" \
 			-e "s;@PREFIX@;$PREFIX;g" \
 			-e "s;@BINDIR@;$BINDIR;g" \
